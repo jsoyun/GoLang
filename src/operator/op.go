af328ce5ed092d39313561a563ef85fd57e50b91
jsoyun <syusmm233@gmail.com>
2021-11-25 14:14:38 +0900

    11연산자

---
+++ src/operator/op.go
@@ -0,0 +1,32 @@
+package main
+
+func main(){
+	// a := 4
+	// b :=   2
+
+	// c := 21
+	// d:= c%10
+	// c = c /10
+	// f := c % 10
+
+// fmt.Printf("첫번째수: %v 두번째수 : %v\n",d,f)
+//   fmt.Printf("a&b =%v\n",a&b)
+//   fmt.Printf("a|b=%v\n", a|b)
+//   fmt.Println("result2=",a^b)
+
+
+// a := 4
+
+// fmt.Println(a<<1)
+// fmt.Println(a>>1)
+
+
+
+
+
+
+
+
+
+
+}
