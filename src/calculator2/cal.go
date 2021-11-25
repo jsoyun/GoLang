6bcae0d249ede0062b556b620a924c56f26b9301
jsoyun <syusmm233@gmail.com>
2021-11-25 16:42:17 +0900

    연산자 계산기 프로그램 코드

---
+++ src/calculator2/cal.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	//프런터
+	"fmt"
+	//한줄 입력했을때 찌꺼기 제거위해
+	"strconv"
+	//문자열을 숫자로 바꿔주는
+
+	"strings"
+
+	//표준입력위해
+	"os"
+	//한줄입력하기 위해
+	"bufio"
+)
+
+
+func main() {
+	fmt.Println("숫자를 입력하세요")
+	reader := bufio.NewReader(os.Stdin)
+	line,_ := reader.ReadString('\n')
+	line= strings.TrimSpace(line)
+
+	n1, _ := strconv.Atoi(line)
+
+	line,_ = reader.ReadString('\n')
+	line = strings.TrimSpace(line)
+	n2,_ := strconv.Atoi(line) 
+
+	fmt.Printf("입력하신 숫자는 %d,%d입니다",n1,n2)
+	fmt.Println("연산자를 입력하세요")
+
+	line,_ =reader.ReadString('\n')
+	line = strings.TrimSpace(line)
+
+	if line == "+" {
+		fmt.Printf("%d+%d=%d",n1,n2,n1+n2)
+	} else if line == "-" {
+		fmt.Printf("%d-%d=%d",n1,n2,n1-n2)
+	} else if line == "*"{
+		fmt.Printf("%d*%d=%d",n1,n2,n1*n2)
+	}else if line == "/" {
+		fmt.Printf("%d/%d=%d",n1,n2,n1/n2)
+	} else {
+		fmt.Println("잘못입력했음")
+	}
+}
--- src/operator/op.go
+++ src/operator/op.go
@@ -1,6 +1,8 @@
 package main
 
-func main(){
+import "fmt"
+
+func main() {
 	// a := 4
 	// b :=   2
 
@@ -9,24 +11,51 @@ func main(){
 	// c = c /10
 	// f := c % 10
 
-// fmt.Printf("첫번째수: %v 두번째수 : %v\n",d,f)
-//   fmt.Printf("a&b =%v\n",a&b)
-//   fmt.Printf("a|b=%v\n", a|b)
-//   fmt.Println("result2=",a^b)
+	// fmt.Printf("첫번째수: %v 두번째수 : %v\n",d,f)
+	//   fmt.Printf("a&b =%v\n",a&b)
+	//   fmt.Printf("a|b=%v\n", a|b)
+	//   fmt.Println("result2=",a^b)
+
+	// a := 4
+
+	// fmt.Println(a<<1)
+	// fmt.Println(a>>1)
+
+	// var a bool = 3 > 4
+
+	// var b bool
+	// b = 3>4
+
+	// var c bool 
+	// c= 3<4 && 2<3
 
 
-// a := 4
+	// fmt.Println(a,b,c)
+//  a:= 3
+// 	if a==5 {
+// 		fmt.Println("참")
+// 	} else if a==4{
+// 		fmt.Println("야야")
+// 	} else {
+// 		fmt.Println("333")
+// 	}
+// 	fmt.Println("끝")
 
-// fmt.Println(a<<1)
-// fmt.Println(a>>1)
 
-
-
-
-
-
-
-
+// a:=5
+// if a==3 || a ==4{
+// 	fmt.Println("a는 3 또는 4이다.")
+// }else{
+// 	fmt.Println("a는 3,4 다 아님")
+// }
+// fmt.Println("a의 값은",a)
+a:=5
+if a<10  && a >2{
+	fmt.Println("a는 10보다 작고 2보다 크다.")
+}else{
+	fmt.Println("a는 10보다 크거나 2보다 작다")
+}
+fmt.Println("a의 값은",a)
 
 
 }
