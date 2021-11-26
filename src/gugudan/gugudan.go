5988c3dbfb75db62aaa1d9f85460682a52980140
jsoyun <syusmm233@gmail.com>
2021-11-26 12:06:38 +0900

    오른쪽화살표삼각형 별

--- src/gugudan/gugudan.go
+++ src/gugudan/gugudan.go
@@ -1,13 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
-
 
 	// //3X2빼고 구구단
 	// for dan := 1; dan <= 9; dan++ {
-		
 
 	// 	fmt.Printf("%d단\n", dan)
 
@@ -22,12 +18,51 @@ func main() {
 	// }
 
 	//별찍기
-	for i := 0; i<5; i++ {
-		for j := 0; j< i+1; j++ {
-			fmt.Print("*")
-		}
-		fmt.Println()
-	}
+	// for i := 0; i<5; i++ {
+	// 	for j := 0; j< i+1; j++ {
+	// 		fmt.Print("*")
+	// 	}
+	// 	fmt.Println()
+	// }
 
+	//별찍기 거꾸로 이건 내가한 방법1
+	// for i:=0; i<5; i++{
+	// 	for j:=5; 0<j-i ; j--{
+	// 		fmt.Print("*")
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	//별찍기 거꾸로 답 방법2
+	// for i:=0; i<5; i++{
+	// 	for j:=0; j<5-i; j++{
+	// 		fmt.Print("*")
+
+	// 	}
+	// 	fmt.Println()
+	// }
+
+
+
+	//별찍기 오른쪽 화살표같은삼각형
+
+	// for i:=0; i<3; i++{
+	// 	for k:=0; k<i+1; k++{
+	// 		fmt.Print("*")
+	// 	}
+	// 	fmt.Println()
+		
+	// }
+
+	// for i := 0; i<2 ; i++ {
+	// 	// fmt.Print("*")
+	// 	for j:=0; i+j<2; j++{
+	// 		fmt.Print("*")
+			
+
+	// 	}
+	// 	fmt.Println()
+
+	// }
 
 }
