229bceb2b6292e4cfc363eccb9da928c5c76d712
jsoyun <syusmm233@gmail.com>
2021-11-26 10:28:50 +0900

    *

--- src/gugudan/gugudan.go
+++ src/gugudan/gugudan.go
@@ -1,6 +1,33 @@
 package main
 
-func main(){
+import "fmt"
 
-	
+func main() {
+
+
+	// //3X2빼고 구구단
+	// for dan := 1; dan <= 9; dan++ {
+		
+
+	// 	fmt.Printf("%d단\n", dan)
+
+	// 	for j := 1; j <= 9; j++ {
+
+	// 		if dan==3&&j ==2 {
+	// 			continue
+	// 		}
+	// 		fmt.Printf("%d*%d=%d\n", dan, j, dan*j)
+	// 	}
+	// 	fmt.Println()
+	// }
+
+	//별찍기
+	for i := 0; i<5; i++ {
+		for j := 0; j< i+1; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+
+
 }
