07cead3be59ddca0fcc14421a8da1085566d0ac9
jsoyun <syusmm233@gmail.com>
2021-11-27 23:37:26 +0900

    15강

--- src/function/func.go
+++ src/function/func.go
@@ -3,16 +3,29 @@ package main
 import "fmt"
 
 
-func add(x int, y int) int{
-	return x+y
-
+func main(){
+	a,b:=fun1(1,3)
+	fmt.Println(a,b)
+}
+func fun1(e,t int)(int,int){
+	fun2(e,t)
+	return e,t
 
 }
-func main(){ 
-	for i:=0; i<10; i++{
-		fmt.Printf("%d+%d=%d\n",i,i+2,add(i,i+2))
 
-	}
+func fun2(e,t int){
+	fmt.Println("func2",e,t)
+}
+// func add(x int, y int) int{
+// 	return x+y
+
+
+// }
+// func main(){ 
+// 	for i:=0; i<10; i++{
+// 		fmt.Printf("%d+%d=%d\n",i,i+2,add(i,i+2))
+
+// 	}
 
 	
-}
+// }
