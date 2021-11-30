33ba617971efa6fa24cd6b315ef75036eb80b94c
jsoyun <syusmm233@gmail.com>
2021-11-30 16:30:21 +0900

    재귀호출

--- src/function/func.go
+++ src/function/func.go
@@ -49,18 +49,37 @@
 
 //재귀호출사용해서 1에서 10까지 합계를 출력하는 프로그램
 
+// package main
+
+// import "fmt"
+// func main(){
+// 	s:=sum(10,0)
+// 	//마지막에 결과를 출력하는
+// 	fmt.Println(s)
+// }
+//        //x s 받고 int를 반환
+// func sum(x int, s int) int {
+// 	if x==0 {
+// 		return s
+// 	}
+// 	s += x
+// 	//sum 다시 호출
+// 	return 	sum(x-1,s)
+// }
+
+//모든 재귀호출 구문은 반복문으로 바꿔줄 수 있어
+//위 재귀호출 반복문으로 바꿔볼게
+
 package main
 
 import "fmt"
+
 func main(){
-	s:=sum(10,0)
+	s := 0
+	for i := 1; i<=10; i++ {
+	s += i
+	}
 	fmt.Println(s)
 }
 
-func sum(x int, s int) int {
-	if x==0 {
-		return s
-	}
-	s += x
-	return 	sum(x-1,s)
-}
+//재귀호출 쉬운 경우 피보나치 수열.. 피보나치 이거야,,f(0)=1 f(1)=1 f(x) = f(x-1)+f(x-2)
